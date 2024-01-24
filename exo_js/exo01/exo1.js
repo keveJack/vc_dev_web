10cc9e1851748d85b7b3f6d33fd0f571a5bf4304
keveJack <kevincooldu36@gmail.com>
2024-01-24 10:46:24 +0100

    exo1 js

---
+++ exo_js/exo01/exo1.js
@@ -0,0 +1,5 @@
+let largeur = prompt("Entrez la largeur du rectangle:");
+let longueur = prompt("Entrez la longueur du rectangle:");
+let aireRectangle = largeur*longueur
+console.log(`L'aire de rectangle est: ${aireRectangle}`);
+
