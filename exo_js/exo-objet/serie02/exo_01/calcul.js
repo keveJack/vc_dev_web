da9f5fb413815de37eb76fb0a3395a8379b334ec
keveJack <kevincooldu36@gmail.com>
2024-02-09 15:58:10 +0100

    object suite

---
+++ exo_js/exo-objet/serie02/exo_01/calcul.js
@@ -0,0 +1,16 @@
+
+class Calcul {
+    constructor(pNum1, pNum2) {
+      this.num1 = pNum1;
+      this.num2 = pNum2;
+    }
+  
+    add() {
+      return this.num1 + this.num2;
+    }
+  
+    multiply() {
+      return this.num1 * this.num2;
+    }
+  }
+  
---
+++ exo_js/exo-objet/serie02/exo_01/main.js
@@ -0,0 +1,24 @@
+
+function lanceCalculAdd() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    
+    const calculator = new Calcul(num1, num2);
+    const result = calculator.add();
+    
+    document.getElementById('result').innerText = `Résultat de l'addition : ${result}`;
+  }
+  
+  function lanceCalculMult() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    
+    const calculator = new Calcul(num1, num2);
+    const result = calculator.multiply();
+    
+    document.getElementById('result').innerText = `Résultat de la multiplication : ${result}`;
+  }
+  
+  document.getElementById('additionBtn').addEventListener('click', lanceCalculAdd);
+  document.getElementById('multiplicationBtn').addEventListener('click', lanceCalculMult);
+  
