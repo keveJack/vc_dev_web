cf667da58166fab3d51c9fe343ff0f4eac46e091
kevin <kevincooldu36@gmail.com>
2024-02-26 11:53:22 +0100

    php

--- PHP/app/tp_surcharge/Personne.php
+++ PHP/app/tp_surcharge/Personne.php
@@ -1,35 +1,41 @@
+
 <?php
 
-class Personne {
+class Personne
+{
     private $prenom;
     private $nom;
 
-    public function __construct($prenom, $nom) {
+    public function __construct($prenom, $nom)
+    {
         $this->prenom = $prenom;
         $this->nom = $nom;
         echo "Constructeur de la classe Personne exécuté <br>";
     }
 
-    public function saluer($message = "Bonjour") {
+    public function saluer($message = "Bonjour")
+    {
         echo "Je m'appelle {$this->prenom} {$this->nom}. $message <br>";
     }
 
-    public function __destruct() {
+    public function __destruct()
+    {
         echo "L'instance de Personne vient d'être détruite <br>";
     }
 
-    public function __debugInfo() {
+    public function __debugInfo()
+    {
         return ['name' => $this->nom, 'first_name' => $this->prenom];
     }
 
-    public function saluerPersonnalise($mot) {
+    public function saluerPersonnalise($mot)
+    {
         echo "$mot {$this->prenom} <br>";
     }
 
-    public function __call($name, $arguments) {
+    public function __call($name, $arguments)
+    {
         echo "La méthode $name a été appelée avec les paramètres suivants : <br>";
         var_dump($arguments);
     }
 }
-
-?>
--- PHP/app/tp_surcharge/index.php
+++ PHP/app/tp_surcharge/index.php
@@ -1,3 +1,4 @@
+
 <?php
 include_once 'functions.php';
 include_once 'Personne.php';
